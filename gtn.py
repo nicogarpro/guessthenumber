82c7bb4e0ecfb0d4c31c334aff7b7930a895c728
NICOGARPRO <69008744+nicogarpro@users.noreply.github.com>
2024-07-21 04:12:13 +0200

    Add files via upload

---
+++ gtn.py
@@ -0,0 +1,43 @@
+import random
+import time
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+number = random.choice(list)
+
+print("Guess a number from 1 to 10\nYou get 3 chances")
+
+num1 = int(input())
+
+if num1 == number:
+    print("Correct!!")
+
+elif num1 > number:
+    print("Lower")
+
+elif num1 < number:
+    print("Higher")
+
+num2 = int(input())
+
+if num2 == number:
+    print("Correct!!")
+
+elif num2 > number:
+    print("Lower")
+
+elif num2 < number:
+    print("Higher")
+
+num3 = int(input())
+
+if num3 == number:
+    print("Correct!!")
+
+elif num3 > number:
+    print("Game over\nThe number was", number)
+
+elif num3 < number:
+    print("Game over\nThe number was", number)
+
+time.sleep(3)
